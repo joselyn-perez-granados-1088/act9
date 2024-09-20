c965e8fd456721a2aa07880ba316e9e846430f99
joselyn-perez-granados-1088 <168213224+joselyn-perez-granados-1088@users.noreply.github.com>
2024-09-20 11:49:04 -0600

    Add files via upload

---
+++ humano1088.py
@@ -0,0 +1,71 @@
+print("act 9 clase humano")
+print("joselyn mat: 22308051281088")
+
+    #zona de class♥
+
+class humano1088:
+
+    #zona de atributos♥
+    edad=0
+    genero=0
+    colordepelo=0
+    estatura=0
+    peso=0
+
+    #zona de funciones dentro de la clase♥
+    def bailar1088(self,n):#bailar
+        print(f"{n} esta bailando e e solo solo e e ")
+    def brincar1088(self,n):#brincar
+        print(f"{n} esta brincando en el trampolin ")
+    def cantar1088(self,n):#cantar
+        print(f"{n} esta cantando con sentimiento ")
+    def comer1088(self,n):#comer
+        print(f"{n} esta comiendo sushi ")
+    #zona de creacion de objetos♥
+joselyn=humano1088()
+jonathan=humano1088()
+
+    #zona de usando objetos♥
+print("resultados de joselyn")   
+print(f"")                      #funciones♥
+joselyn.bailar1088("joselyn")
+joselyn.brincar1088("joselyn")
+joselyn.cantar1088("joselyn")
+joselyn.comer1088("joselyn")
+print(f"")
+print("atributos de joselyn:")    
+print(f"")                     #atributos♥
+joselyn.edad=17
+joselyn.genero="niña"
+joselyn.colordepelo="castaño"
+joselyn.estatura=155
+joselyn.peso=47
+print(f"edad: {joselyn.edad}")
+print(f"genero: {joselyn.genero}")
+print(f"pelo: {joselyn.colordepelo}")
+print(f"estatura: {joselyn.estatura}")
+print(f"peso: {joselyn.peso}")
+
+print(f"")
+
+jonathan.edad=18
+print("resultados de funciones para jonathan ")  
+print(f"")      #funciones♥ 
+jonathan.bailar1088("jonathan")
+jonathan.brincar1088("jonathan")
+jonathan.cantar1088("jonathan")
+jonathan.comer1088("jonathan")
+print(f"")
+
+print("atributos de jonathan:")   
+print(f"")                     #atributos♥
+jonathan.edad=18
+jonathan.genero="niño"
+jonathan.colordepelo="castaño"
+jonathan.estatura=186
+jonathan.peso=80
+print(f"edad: {jonathan.edad}")
+print(f"genero: {jonathan.genero}")
+print(f"pelo: {jonathan.colordepelo}")
+print(f"estatura: {jonathan.estatura}")
+print(f"peso: {jonathan.peso}")
